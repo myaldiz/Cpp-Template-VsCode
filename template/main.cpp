905233f12da467ae7bab17598c5fdd219d7d3970
Mustafa Berk YALDIZ <myaldiz@Mustafas-MacBook-Pro.local>
2018-08-21 15:38:05 +0900

    Initial Commit

---
+++ template/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+void redirect()
+{
+    freopen("out.txt","w",stdout);
+    freopen("in.txt","r",stdin);
+}
+
+using namespace std;
+int main()
+{
+    redirect();
+    return 0;
+}
